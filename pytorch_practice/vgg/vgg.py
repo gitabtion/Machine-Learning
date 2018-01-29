095ba645841973bf4c725b5ac6f81c0e253ba333
abtion <abtion@outlook.com>
2018-01-30 01:22:20 +0800

    update readme

--- pytorch_practice/vgg/vgg.py
+++ pytorch_practice/vgg/vgg.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
 import math
 
+
 __all__ = [
     'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
     'vgg19_bn', 'vgg19',
